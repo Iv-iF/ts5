9096b81664d223d47cc22eba65a205dfd17d3c1a
Iv-iF <igorvotin@gmail.com>
2026-04-03 17:25:49 +0300

    commit 4

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("Это второе важное изменение");
+        int first = 1;
+        System.out.println(first);
     }
 }
