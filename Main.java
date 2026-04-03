ed1e5471408a6b1f8c6b6f7bb5ac65e270319ca1
Iv-iF <igorvotin@gmail.com>
2026-04-03 17:25:19 +0300

    commit 3

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ package ts5;
 class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        System.out.println("Это второе важное изменение");
     }
 }
